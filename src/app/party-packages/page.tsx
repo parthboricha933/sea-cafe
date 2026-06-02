28630bb93373c09e30865bea8b189d8d16fe7838
Z User <z@container>
2026-06-02 13:53:17 +0000

    Add Party Packages page with 4 curated packages, comparison table, WhatsApp booking

---
+++ src/app/party-packages/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function PartyPackages() {
+  return (
+    <iframe
+      src="/party-packages.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        display: 'block',
+      }}
+      title="Party Packages | Bavarchi - The Indian Cuisine Restaurant, Diu"
+    />
+  )
+}
